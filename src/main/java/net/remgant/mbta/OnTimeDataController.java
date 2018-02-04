fb1e69cfffe631f0e1b5ed03ec758e11854110ff
Jeff Remillard <jdr@walrus.home.remgant.net>
2018-02-04 12:28:48 -0500

    Add image request method.

--- src/main/java/net/remgant/mbta/OnTimeDataController.java
+++ src/main/java/net/remgant/mbta/OnTimeDataController.java
@@ -2,6 +2,7 @@ package net.remgant.mbta;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
 
@@ -11,18 +12,34 @@ import java.util.Map;
 @RestController
 public class OnTimeDataController {
 
-    OnTimeDataDAO onTimeDataDAO;
+    private OnTimeDataDAO onTimeDataDAO;
+    private ChartMaker chartMaker;
 
     public OnTimeDataController(OnTimeDataDAO onTimeDataDAO) {
         this.onTimeDataDAO = onTimeDataDAO;
+        this.chartMaker = new ChartMaker(onTimeDataDAO);
     }
 
     @RequestMapping(value = "/on_time_data/{year}/{month}/{day}/{tripId}", method = RequestMethod.GET)
     @ResponseBody
-    public Map<String,Object> processOnTimeDataRequest(@PathVariable("year") int year,
-                                                       @PathVariable("month") int month,
-                                                       @PathVariable("day") int day,
-                                                       @PathVariable("tripId") int tripId) {
-        return onTimeDataDAO.findDataForTrip(LocalDate.of(year,month,day),String.format("CR-Weekday-Fall-17-%03d",tripId));
+    public Map<String, Object> processOnTimeDataRequest(@PathVariable("year") int year,
+                                                        @PathVariable("month") int month,
+                                                        @PathVariable("day") int day,
+                                                        @PathVariable("tripId") int tripId) {
+        return onTimeDataDAO.findDataForTrip(LocalDate.of(year, month, day), tripId);
+    }
+
+    @RequestMapping(value = "/on_time_image/{year}-{month}-{day}-{tripId}.png", method = RequestMethod.GET)
+    @ResponseBody
+    public Object processImageRequest(@PathVariable("year") int year,
+                                      @PathVariable("month") int month,
+                                      @PathVariable("day") int day,
+                                      @PathVariable("tripId") int tripId) {
+        try {
+            return chartMaker.createImageForDateAndTrip(LocalDate.of(year, month, day), tripId, 800, 600);
+        } catch (IOException e) {
+            //TODO return custom error page instead?
+            throw new RuntimeException(e);
+        }
     }
 }
