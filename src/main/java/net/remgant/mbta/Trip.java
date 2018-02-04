b511b4054da5331adf934619c09c1822a0b43752
Jeff Remillard <jdr@walrus.home.remgant.net>
2018-02-04 12:32:26 -0500

    New class to handle trip data.

---
+++ src/main/java/net/remgant/mbta/Trip.java
@@ -0,0 +1,58 @@
+package net.remgant.mbta;
+
+/**
+ * Created by jdr on 2/4/18.
+ */
+public class Trip {
+    int id;
+    String calendar;
+    String type;
+    String route;
+
+    public Trip() {
+    }
+
+    public Trip(String route, int id, String type, String calendar) {
+        this.route = route;
+        this.id = id;
+        this.calendar = calendar;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(String calendar) {
+        this.calendar = calendar;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CR-%s-%s-%03d",type,calendar,id);
+    }
+}
